83bb3c9cb3ab42dc84b0d51058e083de40f7dd31
Rachel-FastSimon <rachel@fastsimon.com>
2023-05-30 12:09:36 +0300

    fullSearch added

---
+++ path/to/script.js
@@ -0,0 +1,65 @@
+const searchForm = document.getElementById('searchForm');
+let searchResults;
+  searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const searchQuery = document.getElementById('searchInput').value;
+    window.FastSimonSDK.fullTextSearch({
+        term: searchQuery,
+        callback: (response) => {
+            console.log(response.payload.products);
+            searchResults = response.payload.products;
+            displaySearchResults(searchResults, searchQuery)
+        }
+    })
+    console.log('Search query:', searchQuery);
+  });
+
+  function displaySearchResults(results, searchQuery) {
+    const searchResultsContainer = document.getElementById('searchResults');
+
+    // Clear previous search results
+    searchResultsContainer.innerHTML = '';
+    const searchResultsTitle = document.createElement('div');
+    searchResultsTitle.classList.add('fs_search_results_title');
+    searchResultsTitle.innerText = `Search results for: "${searchQuery}"`;
+    searchResultsContainer.appendChild(searchResultsTitle);
+    
+    const searchResultsWrapper = document.createElement('div');
+    searchResultsWrapper.classList.add('fs_search_results_wrapper');
+    // Iterate over the results and create the necessary HTML elements
+    results.forEach((product) => {
+      // Create a container for each product
+      const productContainer = document.createElement('div');
+      productContainer.classList.add('fs_product');
+
+      // Add product information to the container
+      const productImage = document.createElement('img');
+      productImage.classList.add('fs_product_image');
+      if(product.t) {
+        productImage.src = product.t;
+      } else {
+        productImage.src = 'https://acp-magento.appspot.com/images/missing.gif';
+      }
+    
+        productContainer.appendChild(productImage);
+
+      if(product.l) {
+        const productName = document.createElement('h3');
+        productName.classList.add('fs_product_title');
+        productName.textContent = product.l;
+        productContainer.appendChild(productName);
+      }
+
+      if(product.p && product.c) {
+        const productPrice = document.createElement('p');
+        productPrice.classList.add('fs_product_price');
+        productPrice.textContent = `${product.p} ${product.c}`;
+        productContainer.appendChild(productPrice);
+      }
+
+
+      // Append the product container to the search results container
+      searchResultsWrapper.appendChild(productContainer);
+      searchResultsContainer.appendChild(searchResultsWrapper);
+    });
+  }
